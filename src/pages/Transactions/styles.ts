b18e0d33f0a095bb26205cf9557ec6656df9677c
Diego Lauar <diego_pktd@hotmail.com>
2025-04-01 15:27:37 -0300

    Finalizando box New Entrada

--- src/pages/Transactions/styles.ts
+++ src/pages/Transactions/styles.ts
@@ -1,17 +1,17 @@
 import styled from "styled-components";
- 
- export const TransactionsContainer = styled.main`
+
+export const TransactionsContainer = styled.main`
    width: 100%;
    max-width: 1120px;
    margin: 4rem auto 0;
    padding: 0 1.5rem;
  `;
- 
- export const TransactionsTable = styled.table`
+
+export const TransactionsTable = styled.table`
    width: 100%;
    border-collapse: separate;
    border-spacing: 0 0.5rem;
-   /* margin-top: 1.5rem; */
+   margin-top: 1.5rem;
  
    td {
      padding: 1.25rem 2rem;
@@ -28,11 +28,11 @@ import styled from "styled-components";
      }
    }
  `;
- 
- interface PriceHighlightProps {
-   variant: "income" | "outcome";
- }
- 
- export const PriceHighlight = styled.span<PriceHighlightProps>`
+
+interface PriceHighlightProps {
+  variant: "income" | "outcome";
+}
+
+export const PriceHighlight = styled.span<PriceHighlightProps>`
    color: ${props => props.variant === "income" ? props.theme["green-300"] : props.theme["red-300"]};
  `;
