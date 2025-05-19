191f8d4bc6bd5d4714b793153633eb1c51db573e
Diego Lauar <diego_pktd@hotmail.com>
2025-05-19 11:55:34 -0300

    feat: Add context selector

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,16 +1,18 @@
-import { useContext } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
 import { TransactionsContext } from "../../contexts/TransactionsContext";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 import { dateFormatter, priceFormatter } from "../../utils/formatter";
+import { useContextSelector } from "use-context-selector";
 
 
 
 export function Transactions() {
 
-    const { transactions } = useContext(TransactionsContext)
+    const transactions = useContextSelector(TransactionsContext, (context) => {
+        return context.transactions
+    })
 
     return (
         <div>
@@ -27,7 +29,7 @@ export function Transactions() {
                                     <td width="50%">{transaction.description}</td>
                                     <td>
                                         <PriceHighlight variant={transaction.type}>
-                                            {transaction.type =='outcome' && '- '}
+                                            {transaction.type == 'outcome' && '- '}
                                             {priceFormatter.format(transaction.price)}
                                         </PriceHighlight>
                                     </td>
